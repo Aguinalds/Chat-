a40a4c7c4191bc1b93ca4f9de24b637263a1575f
Aguinaldo <guimachado1708@gmail.com>
2021-09-02 22:09:19 -0300

    add server

--- app.js
+++ app.js
@@ -2,6 +2,47 @@
 var app = require('./config/server');
 
 /* parametrizar a porta de escuta */
-app.listen(80, function(){
-    console.log("SERVIDOR ON")
+var server = app.listen(80, function () {
+	console.log("SERVIDOR ON")
+})
+
+var io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+/*Criar a conexão por websocket */
+io.on('connection', function (socket) {
+	console.log('Usuario conectou');
+
+	socket.on('disconnect', function () {
+		console.log('Usuario desconectou');
+	});
+
+	socket.on('msgParaServidor', function (data) {
+
+		/* dialogo */
+		socket.emit(
+			'msgParaCliente',
+			{ apelido: data.apelido, mensagem: data.mensagem }
+		);
+
+		socket.broadcast.emit(
+			'msgParaCliente',
+			{ apelido: data.apelido, mensagem: data.mensagem }
+		);
+
+		/*Participantes */
+		if (parseInt(data.apelido_atualizado_nos_clientes) == 0) {
+			socket.emit(
+				'participantesParaCliente',
+				{ apelido: data.apelido }
+			);
+
+			socket.broadcast.emit(
+				'participantesParaCliente',
+				{ apelido: data.apelido }
+			);
+		}
+	});
+
 })
--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -2,14 +2,21 @@ module.exports.iniciaChat = function (application, req, res) {
 
     var dadosForm = req.body;
 
-    req.assert('apelido','Nome ou apelido é necessário' ).notEmpty();   
-    req.assert('apelido','Nome ou apelido deve conter entre 3 a 15' ).len(3, 15);
+    req.assert('apelido', 'Nome ou apelido é necessário').notEmpty();
+    req.assert('apelido', 'Nome ou apelido deve conter entre 3 a 15').len(3, 15);
 
     var erros = req.validationErrors();
 
-    if(erros){
-        res.render("index", {validacao : erros})
+    if (erros) {
+        res.render("index", { validacao: erros })
         return;
     }
-    res.render('chat');
+
+
+    application.get('io').emit(
+        'msgParaCliente',
+        { apelido: dadosForm.apelido, mensagem: 'On-line' }
+    )
+
+    res.render('chat', { dadosForm: dadosForm });
 }
